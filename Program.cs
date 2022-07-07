530d4c3a91d2e21e53760a7c9729732230bd9497
Mikhail <misha-z@yandex.ru>
2022-07-07 23:23:05 +0300

    поправил комментарий

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// программа сравнивает число и квадрат другого числа
+Console.Write( "введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write( "введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+    Console.WriteLine( "a = " + number1 + " " + "b = " + number2  + " " + "да");
+else
+    Console.WriteLine( "a = " + number1 + "b = " + number2 + "нет");
