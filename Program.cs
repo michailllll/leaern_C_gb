79555b352d7d09ff59170b90b5aa22b90a3415b3
Mikhail <misha-z@yandex.ru>
2022-07-10 21:37:18 +0300

    добавил задачу по дз

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿// программа сравнивает число и квадрат другого числа
-Console.Write( "введите число 1: ");
+﻿// Задача 2:  программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write( "напишите число 1: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write( "введите число 2: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 == number2 * number2)
-    Console.WriteLine( "a = " + number1 + " " + "b = " + number2  + " " + "да");
+
+if (number1 > number2)
+   Console.WriteLine( "a = " + number1 + " " + "b = " + number2  + " -> " + "max = " + number1);
 else
-    Console.WriteLine( "a = " + number1 + "b = " + number2 + "нет");
+   Console.WriteLine( "a = " + number1 + " " + "b = " + number2  + " -> " + "max = " + number2);
